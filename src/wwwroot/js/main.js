ff2e688d921a330ad125c029ebcf117582931744
jlbiahdev <jeanlucbiah.dev@outlook.com>
2024-03-13 16:03:47 +0100

    Delivers numbers + testimonials

--- src/wwwroot/js/main.js
+++ src/wwwroot/js/main.js
@@ -5,3 +5,22 @@ $(window).scroll(() => {
         $('.nav-container').removeClass('degrad');
     }
 });
+
+let valueDisplays = document.querySelectorAll('.count');
+let interval = 5000;
+
+valueDisplays.forEach((value) => {
+    let start = 0
+    let end = parseInt(value.getAttribute('data-val'))
+    let duration = Math.floor(interval / end);
+    let counter = setInterval(() => {
+        start++;
+        value.textContent = start;
+
+        if (start == end) {
+            clearInterval(counter)
+        }
+    }, duration)
+
+    console.log(end)
+})
